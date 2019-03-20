5f41638f685f40fd1f905373e14ee7874ea9679f
iamibiswadeep <myselfbiswadeep@gmail.com>
2019-03-20 15:54:56 +0530

    Going to a point and landing

--- Final_Code.py
+++ Final_Code.py
@@ -4,14 +4,6 @@ import time
 import socket,exceptions
 import argparse
 
-'''
-parser = argparse.ArgumentParser(description='By default SITL connects to local PC')
-parser.add_argument('--connection_id', help='connection string for the drone')
-args = parser.parse_args()
-
-connection_string = args.connection_id
-sitl = None
-'''
 
 #if connection string is not entered by the user then
 def connecting_to_vehicle():
@@ -59,7 +51,7 @@ def arm_takeoff_vehicle(Altitude):
 
 	print "Arming the Vehicle"
 	#Vehicle should be in guided mode before TAKE-OFF
-	vehicle.mode=VehicleMode(GUIDED)
+	vehicle.mode=VehicleMode("GUIDED")
 	vehicle.armed=True 
 
 	#take off function allows the vehicle to take to that certain altitude
@@ -68,16 +60,48 @@ def arm_takeoff_vehicle(Altitude):
 	#Restrict any other commands to the vehicle until it reaches the certain height
 
 	while True:
-		print "Altitude: %s" %vehicle.location.global_relative_frame.alt
+		print "Altitude: %s" %vehicle.location.global_relative_frame.alt:
 		# if the below condition doesn't comply with the practical scenarios, then multiply Altitude with 0.95
 		if vehicle.location.global_relative_frame.alt > Altitude
 			print "Altitude Reached"
 			break
-		time.sleep(2)
+		time.sleep(5)
+
+#from one point to another and lands there
+def vehicle_goto_location(land):
+	arm_takeoff_vehicle(10)
+
+	#LocationGlobalRealative(latitude,longitude,altitude)
+	targetPoint1=LocationGlobalRelative(-35.361354,149.165218,20)
+
+	#setting airspeed attribute to 3 to move to that particular point
+	vehicle.airspeed = 3
+
+	vehicle.simple_goto(targetPoint1)
+
+	time.sleep(30) #sleep funtion prevents other meaages to interfere the ongoing function and also in battery saving
+
+	# Now to land the drone in that particular location
+	if land is True:
+		while True:
+			print "Landing.."
+			if vehicle.location.global_relative_frame.alt < 0.12: #if height from ground to drone becomes less than 12 cm
+				vehicle.isarmed = False	
+				print "Succesfully Landed and disarmed motors"
+				break
+	time.sleep(3)
+
+print "Stop Vehicle"
+#CLOSE vehicle object
+vehicle.close()
+
+#Drone goes hrough multiple points
+def vehicle_goto_multiple_points()
+
 
 
 def main:
 	vehicle=connecting_to_vehicle()
-	arm_takeoff_vehicle(100)
+	arm_takeoff_vehicle(input("Enter the altitude:"))
 
 
